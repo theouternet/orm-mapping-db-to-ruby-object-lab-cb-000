93fdf4abf2c7b4a012803d971ad7725ee003e5ae
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 21:37:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,4 +61,20 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE name = 
+    LIMIT 1
+    SQL
+    
+    
+    
+  end 
+  
+  
+  
+  
 end
