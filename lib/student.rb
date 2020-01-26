39f3ad3ebce6ff9e273294aa6cbaa5ad30f4b94c
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-26 21:35:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
     LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name).map.each do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
